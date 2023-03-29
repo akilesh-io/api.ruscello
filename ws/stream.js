bb950b8717ee419352db75dfdd53d585e670a7cb
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2023-03-29 21:44:01 +0530

    feat: :zap: merged all handles in stream.js

--- ws/stream.js
+++ ws/stream.js
@@ -1,37 +1,45 @@
-const stream = ( socket ) => {
-    socket.on( 'subscribe', ( data ) => {
-        //subscribe/join a room
-        socket.join( data.room );
-        socket.join( data.socketId );
+const stream = ( socket ) =>
+{
+  socket.on( 'join', ( data ) =>
+  {
+    //user/join a room
+    socket.join( data.room );
+    //socket.join( data.socketId );
+    //console.log("🚀 ~ file: stream.js:9 ~ socket.on ~ data.socketId:", data.socketId)
 
-        //Inform other members in the room of new user's arrival
-        if ( socket.adapter.rooms.has(data.room) === true ) {
-            socket.to( data.room ).emit( 'new user', { socketId: data.socketId } );
-        }
-    } );
+    //Inform other members in the room of new user's arrival
+    if ( socket.adapter.rooms.has( data.room ) === true )
+    {
+      socket.to( data.room ).emit( 'new', { socketId: data.socketId } );
+    }
+  } );
 
-    socket.on("createdMessage", (msg) => {
-        socket.broadcast.emit("newIncomingMessage", msg);
-    });
+  socket.on( "createdMessage", ( msg ) =>
+  {
+    socket.broadcast.emit( "newIncomingMessage", msg );
+  } );
 
-    socket.on( 'newUserStart', ( data ) => {
-        socket.to( data.to ).emit( 'newUserStart', { sender: data.sender } );
-    } );
-
-
-    socket.on( 'sdp', ( data ) => {
-        socket.to( data.to ).emit( 'sdp', { description: data.description, sender: data.sender } );
-    } );
-
-
-    socket.on( 'ice candidates', ( data ) => {
-        socket.to( data.to ).emit( 'ice candidates', { candidate: data.candidate, sender: data.sender } );
-    } );
+  socket.on( 'videoSeek', ( videoState, data ) =>
+  {
+    socket.broadcast.to( data.room ).emit( 'updateSeek', videoState );
+  } );
 
 
-    socket.on( 'chat', ( data ) => {
-        socket.to( data.room ).emit( 'chat', { sender: data.sender, msg: data.msg } );
-    } );
+  socket.on( 'playPause', ( playPause, data ) =>
+  {
+    socket.broadcast.to( data.room ).emit( 'updatePlayPause', playPause );
+  } );
+
+  socket.on( 'disconnect', () =>
+  {
+    console.log( '🔥: A user disconnected' );
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter( ( user ) => user.socket !== socket.id );
+    // console.log(users);
+    //Sends the list of users to the client
+    socket.emit( 'newUserResponse', users );
+    socket.disconnect();
+  } );
 };
 
 module.exports = stream;
